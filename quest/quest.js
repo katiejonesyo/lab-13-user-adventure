43b11085c1570ab081a81c2b2d1229262b928fbf
Katelinn Jones <katiejonesyo@gmail.com>
2020-10-15 18:59:49 -0700

    buttonnnnnn

--- quest/quest.js
+++ quest/quest.js
@@ -1,2 +1,41 @@
+import quests from '../data.js';
+import { findById } from '../utils.js';
+
+const section = document.querySelector('section');
 const searchParams = new URLSearchParams(window.location.search);
-console.log(searchParams.get('id'));
+const id = searchParams.get('id');
+const quest = findById(quests, id);
+
+const h2 = document.createElement('h2');
+
+h2.textContent = quest.title;
+
+section.appendChild(h2);
+console.log(quest.choices);
+
+
+
+const form = document.createElement('form');
+section.appendChild(form);
+
+quest.choices.forEach(choice => {
+    const label = document.createElement('label');
+    const span = document.createElement('span');
+
+    span.textContent = choice.description;
+
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.value = 'choice.id'
+    radio.name = 'choices';
+
+    label.append(span,radio);
+    form.appendChild(label);
+});
+
+
+const button = document.createElement('button');
+
+button.textContent = 'Confirm';
+
+form.appendChild(button)
