08137a94055eda7c6c9de775a206fc2a048f81a0
Katelinn Jones <katiejonesyo@gmail.com>
2020-10-15 17:57:30 -0700

    local storage,buttons,skeleton of home page

---
+++ app.js
@@ -0,0 +1,19 @@
+import { saveUser } from './storage-utils.js';
+
+const form = document.querySelector('form');
+
+form.addEventListener('cubmit', (e) => {
+    e.preventDefault();
+
+
+    const data = new FormData(form);
+
+   saveUser({
+        name: data.get('name'),
+        class: data.get('class'),
+
+    });
+
+    window.location.href = './map/';
+
+});
